f303c6e36de41bc0b67cd2d0d48ca785286e1f66
Thomas Norris <tom.norris135@gmail.com>
2019-09-09 19:54:46 -0400

    formatting still baaad

--- Project Templates/Lab2_C/main.c
+++ Project Templates/Lab2_C/main.c
@@ -60,13 +60,13 @@ void Delay(void);
 void EnableInterrupts(void);
 
 int main(void){    
-  // TExaS_Init initializes the real board grader
+	// TExaS_Init initializes the real board grader
 	TExaS_Init(SW_PIN_PF40,LED_PIN_PF321); 
-  // Call initialization of port PF4, PF3, PF2, PF1, PF0
-  PortF_Init();
+	// Call initialization of port PF4, PF3, PF2, PF1, PF0
+	PortF_Init();
 	// The grader uses interrupts
-  EnableInterrupts();
-  while(1){
+	EnableInterrupts();
+	while(1){
 		SW1 = GPIO_PORTF_DATA_R&0x10;        // read PF4 into SW1
 		SW2 = GPIO_PORTF_DATA_R&0x01;        // read PF0 into SW2
 		
@@ -88,16 +88,16 @@ int main(void){
 // PF3,PF2,PF1 are outputs to the LED
 // These five pins are connected to hardware on the LaunchPad
 void PortF_Init(void){ volatile unsigned long delay;
-  SYSCTL_RCGC2_R |= 0x00000020;     // 1) F clock
-  delay = SYSCTL_RCGC2_R;           // delay   
-  GPIO_PORTF_LOCK_R = 0x4C4F434B;   // 2) unlock PortF PF0  
-  GPIO_PORTF_CR_R = 0x1F;           // allow changes to PF4-0       
-  GPIO_PORTF_AMSEL_R = 0x00;        // 3) disable analog function
-  GPIO_PORTF_PCTL_R = 0x00000000;   // 4) GPIO clear bit PCTL  
-  GPIO_PORTF_DIR_R = 0x0E;          // 5) PF4,PF0 input, PF3,PF2,PF1 output   
-  GPIO_PORTF_AFSEL_R = 0x00;        // 6) no alternate function
-  GPIO_PORTF_PUR_R = 0x11;          // enable pullup resistors on PF4,PF0       
-  GPIO_PORTF_DEN_R = 0x1F;          // 7) enable digital pins PF4-PF0        
+	SYSCTL_RCGC2_R |= 0x00000020;     // 1) F clock
+	delay = SYSCTL_RCGC2_R;           // delay
+	GPIO_PORTF_LOCK_R = 0x4C4F434B;   // 2) unlock PortF PF0
+	GPIO_PORTF_CR_R = 0x1F;           // allow changes to PF4-0
+	GPIO_PORTF_AMSEL_R = 0x00;        // 3) disable analog function
+	GPIO_PORTF_PCTL_R = 0x00000000;   // 4) GPIO clear bit PCTL
+	GPIO_PORTF_DIR_R = 0x0E;          // 5) PF4,PF0 input, PF3,PF2,PF1 output
+	GPIO_PORTF_AFSEL_R = 0x00;        // 6) no alternate function
+	GPIO_PORTF_PUR_R = 0x11;          // enable pullup resistors on PF4,PF0
+	GPIO_PORTF_DEN_R = 0x1F;          // 7) enable digital pins PF4-PF0
 }
 
 
