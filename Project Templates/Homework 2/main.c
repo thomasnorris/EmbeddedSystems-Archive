78897c162c08f503cfd448a182a01f53740bd4af
Thomas Norris <tom.norris135@gmail.com>
2019-09-10 18:20:19 -0400

    imported homework 2

---
+++ Project Templates/Homework 2/main.c
@@ -0,0 +1,24 @@
+/*******************************************************************
+ *  main.c
+ *  This is an embedded program template.
+ *  
+ *  Author: G. Serpen
+ *  Date: Date: May 22, 2017
+ *
+ *  
+ *******************************************************************/
+
+
+
+
+// Called by startup assembly code, start of C code
+int main(void){    
+	
+	
+	
+  while(1){
+		
+  }
+}
+
+
