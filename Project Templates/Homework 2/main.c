dd720f370c2e2ee080d94baebd6d3997cec28f6a
Thomas Norris <tom.norris135@gmail.com>
2019-09-10 18:30:11 -0400

    added code fragment function skeletons for problem 1

--- Project Templates/Homework 2/main.c
+++ Project Templates/Homework 2/main.c
@@ -2,23 +2,37 @@
  *  main.c
  *  This is an embedded program template.
  *  
- *  Author: G. Serpen
- *  Date: Date: May 22, 2017
+ *  Author: Thomas Norris
+ *  Date: Date: 09/10/19
  *
  *  
  *******************************************************************/
 
-
-
-
 // Called by startup assembly code, start of C code
-int main(void){    
-	
-	
-	
-  while(1){
+
+void checkBitValue(void);
+void setBitValue(void);
+void clearBitValue(void);
+void toggleBitValue(void);
+
+int main(void) {
+	while(1) {
 		
-  }
+	}
 }
 
+void checkBitValue(void) {
 
+}
+
+void setBitValue(void) {
+
+}
+
+void clearBitValue(void) {
+
+}
+
+void toggleBigValue(void) {
+
+}
