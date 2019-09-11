39dabb268e9b31d1d8ec597c1fc83caa5ea121d7
Thomas Norris <tom.norris135@gmail.com>
2019-09-10 22:24:55 -0400

    finish problem 2

--- Project Templates/Homework 2/main.c
+++ Project Templates/Homework 2/main.c
@@ -11,8 +11,17 @@
 // Called by startup assembly code, start of C code
 
 int main(void) {
-
+	int start_val = 5;
+	int factorial = 1;
+	
+	while (start_val) {
+		factorial *= start_val;
+		--start_val;
+	}
+	
+	// do somthing with factorial
+	
 	while(1) {
-		
+		// do nothing
 	}
 }
