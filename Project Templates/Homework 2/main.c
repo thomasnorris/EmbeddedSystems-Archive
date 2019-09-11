6cacbe59891032f3cc76f17e58cedff3b123e1ee
Thomas Norris <tom.norris135@gmail.com>
2019-09-11 18:39:41 -0400

    program 4 done

--- Project Templates/Homework 2/main.c
+++ Project Templates/Homework 2/main.c
@@ -14,6 +14,7 @@ void updateCreditRatingAlice(int opCode);
 int rewardsOrAlarm(int index);
 
 // Note: I prepend globals with _
+// Note: I am assuming that alice starts with a rating of 750, and _monthlyPaymentHistoryAlice[0] is for the next month
 int _creditStatus;
 int _creditRatingAlice = 750;
 int _maxCreditRating = 800;
@@ -24,7 +25,7 @@ int main(void) {
 	int index = 0;
 	int arraySize = sizeof(_monthlyPaymentHistoryAlice) / sizeof(int);
 	while (1) {
-		if (index == arraySize)
+		if (index == arraySize)                                      // reset the index back to zero to restart the cycle
 			index = 0;
 		_creditStatus = rewardsOrAlarm(index);
 		index++;
@@ -32,9 +33,9 @@ int main(void) {
 }
 
 void updateCreditRatingAlice(int opCode) {
-	if (opCode == 1 && _creditRatingAlice != _maxCreditRating)
+	if (opCode == 1 && _creditRatingAlice != _maxCreditRating)       // only add 10 to the credit rating if it isn't already max
 		_creditRatingAlice += 10;
-	else if (_creditRatingAlice != _minCreditRating)
+	else if (_creditRatingAlice != _minCreditRating)                 // only subtract 10 from the credit rating if it isn't already min
 		_creditRatingAlice -= 10;
 }
 
@@ -47,9 +48,9 @@ int rewardsOrAlarm(int index) {
 	newCreditRating = _creditRatingAlice;
 	
 	if (newCreditRating == previousCreditRating) {
-		if (newCreditRating == _maxCreditRating)
+		if (newCreditRating == _maxCreditRating)                     // if the previous month's credit rating didn't change and it's max, return 1
 			return 1;
-		else if (newCreditRating == _minCreditRating)
+		else if (newCreditRating == _minCreditRating)                // if the previous month's credit rating didn't change and it's minimum, return 0
 			return 0;
 	}
 	
