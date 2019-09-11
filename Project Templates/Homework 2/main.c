38fac974da347d61f79d30cde29bf3ea8a22130d
Thomas Norris <tom.norris135@gmail.com>
2019-09-10 21:49:51 -0400

    clearing code for next problem

--- Project Templates/Homework 2/main.c
+++ Project Templates/Homework 2/main.c
@@ -10,50 +10,9 @@
 
 // Called by startup assembly code, start of C code
 
-void checkBitValue(void);
-void setBitValue(void);
-void clearBitValue(void);
-void toggleBitValue(void);
-
 int main(void) {
 
 	while(1) {
-		checkBitValue();
-		setBitValue();
-		clearBitValue();
-		toggleBitValue();
+		
 	}
-}
-
-void checkBitValue(void) {
-	char ch1 = 0x34;      // 0x34 = 2_00110100
-	char mask = 0x20;     // 0x20 = 2_00100000
-	char ch2;
-	
-	if (ch1 & mask)       // yeilds 2_00100000
-		ch2 = 1;
-	else
-		ch2 = 0;
-}
-
-void setBitValue(void) {
-	char ch1 = 0x34;      // 0x34 = 2_00110100
-	char mask = 0x80;     // 0x80 = 2_10000000
-	
-	ch1 |= mask;          // sets ch1 to 2_10110100
-}
-
-void clearBitValue(void) {
-	char ch1 = 0x34;      // 0x34 = 2_00110100
-	char mask = 0x34;     // 0x34 = 2_00110100
-	char ch2;
-	
-	ch1 &= mask;          // sets ch1 to 2_00110100 (bit 3 is already 0 so there is no change)
-}
-
-void toggleBitValue(void) {
-	char ch1 = 0x34;      // 0x34 = 2_00110100
-	char mask = 0x16;     // 0x16 = 2_00010000
-	
-	ch1 ^= mask;          // sets ch1 to 2_0010010
 }
