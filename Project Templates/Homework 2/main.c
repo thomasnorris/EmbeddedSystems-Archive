d7de8e9f125c6f44223ea117c23186045d43307c
Thomas Norris <tom.norris135@gmail.com>
2019-09-11 11:04:10 -0400

    globals

--- Project Templates/Homework 2/main.c
+++ Project Templates/Homework 2/main.c
@@ -10,12 +10,13 @@
 
 // Called by startup assembly code, start of C code
 
+int maxValueArray[10] = {-1, 5, 3, 8, -10, 23, 6, 5, 2, 10};
+int arraySize = 10;
+
 int main(void) {
-	int maxValueArray[10] = {-1, 5, 3, 8, -10, 23, 6, 5, 2, 10};
-	int arraySize = 10;
 	int maxValue = 0;
-	
 	int i;
+	
 	for (i = 0; i < arraySize; ++i) {
 		int currentValue = maxValueArray[i];
 		if (currentValue >= maxValue)
