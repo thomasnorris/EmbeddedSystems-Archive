f55daf52d25e7770f118a1858e704a518ff8cd6a
Thomas Norris <tom.norris135@gmail.com>
2019-09-20 08:49:25 -0400

    port init functions without proper port hex value

--- Project Templates/Homework 3/main.c
+++ Project Templates/Homework 3/main.c
@@ -7,12 +7,21 @@
 #include <stdint.h>
 #include "inc/tm4c123gh6pm.h"
 
-void PortE_Init(void);
+//void PortE_Init(void);
+void initPortA(void);
+void initPortB(void);
+void initPortC(void);
+void initPortD(void);
 
 int main(void){
 	
 	// init port E
-	PortE_Init();
+	// PortE_Init();
+	
+	initPortA();
+	initPortB();
+	initPortC();
+	initPortD();
 	
 	// forever loop
 	while(1) {
@@ -20,10 +29,53 @@ int main(void){
 	}
 }
 
+void initPortA(void) {
+	unsigned long portHex = 0x00;
+	
+	SYSCTL_RCGCGPIO_R |= portHex;
+	while (!(SYSCTL_PRGPIO_R & portHex)) {
+	}
+	
+	GPIO_PORTA_LOCK_R = GPIO_LOCK_KEY;
+	GPIO_PORTA_PCTL_R = 0x00;
+}
+
+void initPortB(void) {
+	unsigned long portHex = 0x00;
+	
+	SYSCTL_RCGCGPIO_R |= portHex;
+	while (!(SYSCTL_PRGPIO_R & portHex)) {
+	}
+	
+	GPIO_PORTB_LOCK_R = GPIO_LOCK_KEY;
+	GPIO_PORTB_PCTL_R = 0x00;
+}
+
+void initPortC(void) {
+	unsigned long portHex = 0x00;
+	
+	SYSCTL_RCGCGPIO_R |= portHex;
+	while (!(SYSCTL_PRGPIO_R & portHex)) {
+	}
+	
+	GPIO_PORTC_LOCK_R = GPIO_LOCK_KEY;
+	GPIO_PORTC_PCTL_R = 0x00;
+}
+
+void initPortD(void) {
+	unsigned long portHex = 0x00;
+	
+	SYSCTL_RCGCGPIO_R |= portHex;
+	while (!(SYSCTL_PRGPIO_R & portHex)) {
+	}
+	
+	GPIO_PORTD_LOCK_R = GPIO_LOCK_KEY;
+	GPIO_PORTD_PCTL_R = 0x00;
+}
+
 void PortE_Init(void) {
 	SYSCTL_RCGCGPIO_R |= 0x10;
 	while (!(SYSCTL_PRGPIO_R & 0x10)) {
-		// wait for the clock to be active, do nothing in the mean time
 	}
 	
 	GPIO_PORTE_LOCK_R = GPIO_LOCK_KEY;
