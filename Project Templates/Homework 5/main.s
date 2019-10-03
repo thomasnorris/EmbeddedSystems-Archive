cd262ea0939168a56f37b56479566746831675e5
Thomas Norris <tom.norris135@gmail.com>
2019-10-03 16:15:54 -0400

    import project

---
+++ Project Templates/Homework 5/main.s
@@ -0,0 +1,26 @@
+;*******************************************************************
+; main.s
+; Author:
+; Date Created:
+; Last Modified:
+; Section Number:
+; Instructor: 
+; Homework Number: 5
+;   Brief description of the program
+;
+;*******************************************************************
+
+
+
+       AREA    |.text|, CODE, READONLY, ALIGN=2
+       THUMB
+       EXPORT  Start
+Start
+
+
+loop   B    loop
+
+
+       ALIGN      ; make sure the end of this section is aligned
+       END        ; end of file
+       
