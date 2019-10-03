9a035e37a8371033ac1e8a42bc89aff6113acf90
Thomas Norris <tom.norris135@gmail.com>
2019-10-03 16:21:36 -0400

    comments and setup

--- Project Templates/Homework 5/main.s
+++ Project Templates/Homework 5/main.s
@@ -1,24 +1,27 @@
 ;*******************************************************************
 ; main.s
-; Author:
-; Date Created:
-; Last Modified:
-; Section Number:
-; Instructor: 
+; Author: Thomas Norris
+; Date Created: 10/3/19
+; Last Modified: 10/3/19
+; Section Number: 003
+; Instructor: Gursel Serpen
 ; Homework Number: 5
-;   Brief description of the program
-;
+; Brief description of the program
+;	Program for HW5	
 ;*******************************************************************
 
 
 
-       AREA    |.text|, CODE, READONLY, ALIGN=2
-       THUMB
-       EXPORT  Start
+	AREA    |.text|, CODE, READONLY, ALIGN=2
+	THUMB
+	EXPORT  Start
+
 Start
+	; do stuff, fall to loop
 
-
-loop   B    loop
+loop   
+	; do more stuff
+	B    loop
 
 
        ALIGN      ; make sure the end of this section is aligned
