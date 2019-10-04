7032859979ff80e998d638943e035256128eda61
Thomas Norris <tom.norris135@gmail.com>
2019-10-04 15:44:28 -0400

    first two byte replacements

--- Project Templates/Homework 5/main.s
+++ Project Templates/Homework 5/main.s
@@ -7,7 +7,7 @@
 ; Instructor: Gursel Serpen
 ; Homework Number: 5
 ; Brief description of the program
-;	Program for HW5	
+;	Program for HW5
 ;*******************************************************************
 
 
@@ -17,13 +17,22 @@
 	EXPORT  Start
 
 Start
-	; do stuff, fall to loop
+	; do stuff
+	; not tested
+	; question 1
+	MOV r1, #0x12345678     ; 32 bit test value
+	MOV r0, r1, LSL #24     ; r0 = 0x78000000
 
-loop   
-	; do more stuff
+	MOV r2, r1, LSR #8      ; r2 = 0x00123456
+	MOV r2, r2, LSL #24     ; r2 = 0x56000000
+	MOV r2, r2, LSR #8      ; r2 = 0x00560000
+
+	ORR r0, r0, r2          ; r0 = 0x78560000
+
+loop
+	; forever
 	B    loop
 
 
        ALIGN      ; make sure the end of this section is aligned
        END        ; end of file
-       
