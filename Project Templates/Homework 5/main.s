f5dab1b5c1255dd308a68006f5563304f18ba032
Thomas Norris <tom.norris135@gmail.com>
2019-10-04 16:14:59 -0400

    q1 done without subroutine or keil

--- Project Templates/Homework 5/main.s
+++ Project Templates/Homework 5/main.s
@@ -10,15 +10,12 @@
 ;	Program for HW5
 ;*******************************************************************
 
-
-
 	AREA    |.text|, CODE, READONLY, ALIGN=2
 	THUMB
 	EXPORT  Start
 
 Start
-	; do stuff
-	; not tested
+	; not tested using KEIL
 	; question 1
 	MOV r1, #0x12345678     ; 32 bit test value
 	MOV r0, r1, LSL #24     ; r0 = 0x78000000
@@ -26,8 +23,15 @@ Start
 	MOV r2, r1, LSR #8      ; r2 = 0x00123456
 	MOV r2, r2, LSL #24     ; r2 = 0x56000000
 	MOV r2, r2, LSR #8      ; r2 = 0x00560000
-
 	ORR r0, r0, r2          ; r0 = 0x78560000
+
+	MOV r2, r1, LDR #16     ; r2 = 0x00001234
+	MOV r2, r2, LSL #24     ; r2 = 0x34000000
+	MOV r2, r2, LSR #16     ; r2 = 0x00003400
+	ORR r0, r0, r2          ; r0 = 0x78563400
+
+	MOV r2, r1, LSR #24     ; r2 = 0x00000012
+	ORR r0, r0, r2          ; r0 = 0x78563412
 
 loop
 	; forever
