b1553482b1af0811c12f98b00e7fd0cce6ca51d2
Thomas Norris <tom.norris135@gmail.com>
2019-10-04 16:59:31 -0400

    small formatting change

--- Project Templates/Homework 5/main.s
+++ Project Templates/Homework 5/main.s
@@ -15,11 +15,10 @@
 	EXPORT  Start
 
 Start
-	; question 1
 	; note: the test value can be any 8-digit hex value, not just what's here. I used
 	;       0x12345678 when writing the instructions but that value is too big to be executed
 	;       on the controller.
-	
+
 	MOV r1, #0x00110011     ; 32 bit test value
 	MOV r0, r1, LSL #24     ; r0 = 0x11000000
 
@@ -30,22 +29,23 @@ Start
 	MOV r2, #16
 	MOV r3, #24
 	BL chopAndShift         ; r0 = 0x11001100 after execution
-	
+
 	MOV r3, r1, LSR #24     ; r3 = 0x00000000
 	ORR r0, r0, r3          ; r0 = 0x11001100
-	
-	B loop                  ; branch to forever loop
+
+	B loop                  ; branch (without link) to forever loop
 
 chopAndShift
 	; r0, r1, r2, r3 are parameters
 	; r4 is a local variable
 	; returns in r0
+
 	MOV r4, r1, LSR r2      ; chop right r2 bits off r1, store in r4
 	MOV r4, r4, LSL r3      ; chop left r3 bits off r4, store in r4
 	MOV r4, r4, LSR r2      ; shift r4 right by r2 bits, store in r4
-	
+
 	ORR r0, r0, r4          ; bitwise OR r4 byte with r0
-	
+
 	BX LR                   ; branch back to the link register
 
 loop
