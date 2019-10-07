5b06a0440b79fd87eba461e7deb3cf6bc8b2743c
Thomas Norris <tom.norris135@gmail.com>
2019-10-07 19:08:00 -0400

    done

--- Project Templates/Homework 5/main.s
+++ Project Templates/Homework 5/main.s
@@ -15,7 +15,19 @@
 	EXPORT  Start
 
 Start
-	; do stuff, fall to loop
+	MOV r0, #0             ; r0 will be the result
+	MOV r1, #-4            ; signed int to raise to a power
+	MOV r2, #3             ; the unsigned power which to raise r1
+
+	MOV r0, r1             ; move r1 into r0 (same as multiplying r1 by 1)
+
+multiply
+	CMP r2, #1             ; if r2 = 1, we are done, go to loop
+	BEQ loop
+	
+	MUL r0, r0, r1         ; multiply r0 by r1 and place in r0
+	SUB r2, r2, #1         ; subtract 1 from r2
+	B multiply             ; do the multiplication again
 
 loop   
 	; forever
