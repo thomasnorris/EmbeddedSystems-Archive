3434f0da987b7fd91199c68546d68ff87ca35a9d
Thomas Norris <tom.norris135@gmail.com>
2019-10-07 12:51:29 -0400

    sum is calculated; tested; still need to change registers

--- Project Templates/Homework 5/main.s
+++ Project Templates/Homework 5/main.s
@@ -15,12 +15,13 @@
 	EXPORT  Start
 
 Start
-	; untested
-	LDR r1, intArrLength
-	SUB r1, r1, intArrLength        ; r1 = intArrLength - 1
+	; TODO: change registers so r0 contains the sum to be returned from forLoop
+	
+	LDR r0, =intArr                 ; load r0 with the base address of intArr
+	LDR r1, intArrLength            ; set r1 to intArrLength
+	SUB r1, r1, #1                  ; subtract 1 from r1 so r1 = intArrLength - 1
 	MOV r2, #0                      ; r2 will hold the sum of the array, initialize to 0
 	MOV r3, #0                      ; r3 will hold the current index of the array, initialize to 0
-	LDR r0, =intArr                 ; load r0 with the base address of intArr
 
 forLoop
 	CMP r3, r1                      ; compare r1 with r3
@@ -28,7 +29,9 @@ forLoop
 
 	PUSH {r4}                       ; we need r4, push it to the stack
 
-	; TODO: Code here
+	LDR r4, [r0], #4                ; load the value of r3 into r4 and shift the array by 4 to get the next array value
+	ADD r2, r2, r4                  ; add r4 to the running sum in r2
+	ADD r3, r3, #1                  ; add 1 to the current array index in r3
 
 	POP {r4}                        ; pop r4 from the stack
 
