90abf1f63a9b532ab7ce13568ed2ddf92e77ff79
Thomas Norris <tom.norris135@gmail.com>
2019-10-14 08:21:40 -0400

    import project template for hw6

---
+++ Project Templates/Homework 6/main.s
@@ -0,0 +1,26 @@
+;*******************************************************************
+; main.s
+; Author: Thomas Norris
+; Date Created: 10/14/19
+; Last Modified: 10/14/19
+; Section Number: 003
+; Instructor: Gursel Serpen
+; Homework Number: 6
+; Brief description of the program
+;	Program for HW6
+;*******************************************************************
+
+	AREA    |.text|, CODE, READONLY, ALIGN=2
+	THUMB
+	EXPORT  Start
+
+Start
+	; do stuff, fall to loop
+
+loop
+	; forever
+	B    loop
+
+
+       ALIGN      ; make sure the end of this section is aligned
+       END        ; end of file
