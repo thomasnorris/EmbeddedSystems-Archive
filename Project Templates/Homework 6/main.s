7344aa6bb588c993c0a69bb6a7a4c611645895ea
Thomas Norris <tom.norris135@gmail.com>
2019-10-14 08:33:10 -0400

    setup for q1; untested

--- Project Templates/Homework 6/main.s
+++ Project Templates/Homework 6/main.s
@@ -15,12 +15,30 @@
 	EXPORT  Start
 
 Start
-	; do stuff, fall to loop
+	; use registers r0 to r3 for first 4 parameters
+	MOV r0, #1
+	MOV r1, #2
+	MOV r2, #3
+	MOV r3, #4
+
+	; push last 6 numbers to the stack
+
+	; call subroutine
+	BL ComputeSum
+
+	; do something with the sum in r0
+
+	B loop
+
+ComputeSum
+	; sum all 10 numbers are return
+
+	BX LR
 
 loop
 	; forever
 	B    loop
 
 
-       ALIGN      ; make sure the end of this section is aligned
-       END        ; end of file
+	ALIGN      ; make sure the end of this section is aligned
+	END        ; end of file
