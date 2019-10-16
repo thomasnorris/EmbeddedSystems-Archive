7cdb8c8af84f733d76bf57b5b342dbfc62b06be9
Thomas Norris <tom.norris135@gmail.com>
2019-10-16 18:14:00 -0400

    done

--- Project Templates/Homework 6/main.s
+++ Project Templates/Homework 6/main.s
@@ -15,12 +15,39 @@
 	EXPORT  Start
 
 Start
-	; do stuff, fall to loop
+	LDR r0, =strToProcess     ; load the address of the string into r0
+	LDR r1, strLen            ; load the length of the string into r1
+	MOV r2, #0                ; r2 will be the index counter, initialize to 0
+	MOV r3, #3                ; load the shift amount into r3
+
+CaesarShift
+	ADD r2, r2, #1            ; increment index counter
+	PUSH {r4, r5}             ; need r4 and r5, push to the stack
+	
+	LDRB r4, [r0]             ; load the ascii value of the next byte
+	ADD r4, r4, r3            ; add r3 to r4 to get the next ascii value
+	STRB r4, [r0]             ; store the byte in r4 back into r0 at the address
+	LDR r5, [r0], #1          ; increment r0 address by 1 byte (r5 gets thrown away)
+	
+	POP {r4, r5}              ; pop r4 and r5
+	
+	CMP r2, r1                ; compare r2 and r1
+	BNE CaesarShift           ; branch only if the entire string has been processed
+	
+	; data at address at r0 will contain the shifted string
 
 loop
 	; forever
 	B    loop
 
+	AREA    |.text|, DATA, READONLY, ALIGN=2
+		
 
-       ALIGN      ; make sure the end of this section is aligned
-       END        ; end of file
+strLen
+	DCD 8
+strToProcess
+	DCB "shift me",0
+	
+	
+	ALIGN      ; make sure the end of this section is aligned
+	END        ; end of file
