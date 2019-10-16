e22fb9e531f24e181be4cc9eddae666583c240d5
Thomas Norris <tom.norris135@gmail.com>
2019-10-16 18:37:21 -0400

    done

--- Project Templates/Homework 6/main.s
+++ Project Templates/Homework 6/main.s
@@ -7,7 +7,7 @@
 ; Instructor: Gursel Serpen
 ; Homework Number: 6
 ; Brief description of the program
-;	Program for HW6
+;	Program for HW6 - Question 3
 ;*******************************************************************
 
 	AREA    |.text|, CODE, READONLY, ALIGN=2
@@ -15,12 +15,23 @@
 	EXPORT  Start
 
 Start
-	; do stuff, fall to loop
+	MOV r0, #13          ; store the age in r0
+
+MoviePrice
+	CMP r0, #12
+	MOVLE r0, #6         ; ticket price is $6 for those ages 12 and under, store in r0 and exit
+	BLE loop
+	
+	CMP r0, #65
+	MOVGE r0, #7         ; ticket price is $7 for those ages 65 or over, store in r0 and exit
+	BGE loop
+	
+	MOV r0, #8           ; ticket price is $8 for all other ages, store in r0 and exit
 
 loop
 	; forever
 	B    loop
 
 
-       ALIGN      ; make sure the end of this section is aligned
-       END        ; end of file
+	ALIGN      ; make sure the end of this section is aligned
+	END        ; end of file
