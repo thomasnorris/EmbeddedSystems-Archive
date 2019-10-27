128c092d9dc07f4b250ed4525a6a65a36c93d39f
Thomas Norris <tom.norris135@gmail.com>
2019-10-27 19:28:46 -0400

    started conversion of lab 6 to hw7

---
+++ Project Templates/Homework 7/main.c
@@ -0,0 +1,17 @@
+// Program written by: Thomas Norris
+// Date Created: 10/27/19
+// Last Modified: 10/27/19
+// Lab Section: 003
+// Lab Instructor: Gursel Serpen
+// Homework Number: 7
+
+
+#include <stdint.h>
+#include "inc/tm4c123gh6pm.h"
+
+int main() {
+
+	while(1) {
+	
+	}
+}
