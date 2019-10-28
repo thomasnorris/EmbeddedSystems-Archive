db25529497b7eb974617d1c6bf808a8ac62c5a27
Thomas Norris <tom.norris135@gmail.com>
2019-10-27 20:28:22 -0400

    question 1a done

--- Project Templates/Homework 7/main.c
+++ Project Templates/Homework 7/main.c
@@ -3,17 +3,47 @@
 // Last Modified: 10/27/19
 // Lab Section: 003
 // Lab Instructor: Gursel Serpen
-// Homework Number: 7
-
+// Homework Number: 7 - Question 1a
 
 #include <stdint.h>
 #include "inc/tm4c123gh6pm.h"
 
+// include SysTick functions from SysTick.s
 extern void SysTick_Init(void);
+extern void SysTick_Wait(void);
+
+void waitForMs(uint32_t);
+void sysTickInit(void);
 
 int main() {
-
-	while(1) {
+	uint32_t delayInMs = 1;
 	
+	// initialize SysTick
+	sysTickInit();
+	
+	while (1) {
+		// wait a desired time in ms
+		waitForMs(delayInMs);
+		// do something after the wait
 	}
+}
+
+
+void waitForMs(uint32_t delayInMs) {
+	uint32_t oneMs = 50000;                     // 1 ms / 20 ns = 50000 counts
+	delayInMs *= oneMs;                         // total delay
+	NVIC_ST_RELOAD_R = delayInMs - 1;           // set the reload register
+	NVIC_ST_CURRENT_R = 0;                      // clear the current value
+	
+	SysTick_Wait();                             // wait delayInMs
+}
+
+void sysTickInit(void) {
+	// setting values stated in the question
+	NVIC_ST_CTRL_R = 0;
+	NVIC_ST_RELOAD_R = 0x00FFFFFF;
+	NVIC_ST_CURRENT_R = 0;
+	NVIC_ST_CTRL_R = 0x05;
+	
+	SysTick_Init();
 }
