bca5f1502e46d81d8a8c18290287cbfbac135c51
Thomas Norris <tom.norris135@gmail.com>
2019-10-28 23:13:51 -0400

    getting ready

--- Project Templates/Homework 7/main.c
+++ Project Templates/Homework 7/main.c
@@ -3,13 +3,13 @@
 // Last Modified: 10/27/19
 // Lab Section: 003
 // Lab Instructor: Gursel Serpen
-// Homework Number: 7
+// Homework Number: 7 - Question 7
 
 
 #include <stdint.h>
 #include "inc/tm4c123gh6pm.h"
 
-extern void SysTick_Init(void);
+extern void Swap(void);
 
 int main() {
 
---
+++ Project Templates/Homework 7/swap.s
@@ -0,0 +1,20 @@
+; Program written by: Thomas Norris
+; Date Created: 10/27/19
+; Last Modified: 10/27/19
+; Lab Section: 003
+; Lab Instructor: Gursel Serpen
+; Homework Number: 7 - Question 7
+
+
+	AREA subroutine, CODE
+	ALIGN
+
+; r0 = 
+Swap PROC
+	EXPORT Swap
+loop
+
+	BX LR
+	
+	ENDP
+	END
