7d560d916f16b00b1a15c09f15c5f461ec05ce90
Thomas Norris <tom.norris135@gmail.com>
2019-10-28 23:14:41 -0400

    assembly friendly comments

--- Project Templates/Homework 7/RemoveAllCharFromStr.s
+++ Project Templates/Homework 7/RemoveAllCharFromStr.s
@@ -1,9 +1,9 @@
-// Program written by: Thomas Norris
-// Date Created: 10/27/19
-// Last Modified: 10/27/19
-// Lab Section: 003
-// Lab Instructor: Gursel Serpen
-// Homework Number: 7 - Question 6
+; Program written by: Thomas Norris
+; Date Created: 10/27/19
+; Last Modified: 10/27/19
+; Lab Section: 003
+; Lab Instructor: Gursel Serpen
+; Homework Number: 7 - Question 6
 
 
 	AREA subroutine, CODE
